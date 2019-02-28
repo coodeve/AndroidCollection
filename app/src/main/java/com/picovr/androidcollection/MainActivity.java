d2d788367a6b5012e9a69fc31b17c4ad58926134
patrick.ding <patrick.ding@picovr.com>
2019-02-28 10:50:34 +0800

    add some widget

--- app/src/main/java/com/picovr/androidcollection/MainActivity.java
+++ app/src/main/java/com/picovr/androidcollection/MainActivity.java
@@ -3,27 +3,17 @@ package com.picovr.androidcollection;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
-<<<<<<< HEAD
+
 
 public class MainActivity extends Activity {
     private final static String TAG = MainActivity.class.getSimpleName();
     private Button button;
-=======
-import android.widget.TextView;
 
-
-public class MainActivity extends Activity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
-
-    private String[] pathArr = new String[]{"a","b","c","e","f","g","h","i","j","k"};
-
-    private int index;
-
-    private Button add;
 
 
     @Override
@@ -31,18 +21,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
-        add = findViewById(R.id.add);
-    }
-
-    private String randomStr(){
-        if(index <10){
-            return pathArr[index++];
-        }
-            return "";
     }
 
 }
