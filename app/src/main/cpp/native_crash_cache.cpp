0191966cb757ec142faed0b0af14757fd25b08a0
patrick.ding <patrick.ding@picovr.com>
2020-07-22 15:10:47 +0800

    add native crash

--- app/src/main/cpp/native_crash_cache.cpp
+++ app/src/main/cpp/native_crash_cache.cpp
@@ -10,17 +10,13 @@
 #include <pthread.h>
 #include <dlfcn.h>
 #include <unistd.h>
+#include "Logger.h"
 
-
-
-void callback(Thread *thread, void *p) {
-
-}
 
 void get_native_thread() {
     void *handle = dlopen("libart.so", RTLD_LAZY);
-    void (*foreach)()
-    dlsym(handle, "ForEach")
+    void (*foreach)();
+    dlsym(handle, "ForEach");
 }
 
 
@@ -111,27 +107,187 @@ void nativeInit(JNIEnv *env, jclass javaClass, jstring packageNameStr, jstring t
 }
 
 
-// sigaction 监听
-void initSigaction() {
-    struct sigaction sa;
-    struct sigaction sa_old;
-    memset(&sa, 0, sizeof(sa));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = my_handler;
-    sa.sa_flags = SA_SIGINFO;
-    if (sigaction(SIGABRT, &sa, &sa_old) == 0) {
-
+/**
+ * 获取线程名称
+ * 然后把线程名称传给Java层
+ * @param tid
+ * @return
+ */
+char *getThreadName(pid_t tid) {
+    if (tid <= 1) {
+        return NULL;
     }
+    size_t THREAD_NAME_LENGTH = 100;
+
+    char *path = (char *) calloc(1, 80);
+    char *line = (char *) calloc(1, THREAD_NAME_LENGTH);
+
+    snprintf(path, PATH_MAX, "proc/%d/comm", tid);
+    FILE *commFile = NULL;
+    if (commFile = fopen(path, "r")) {
+        fgets(line, THREAD_NAME_LENGTH, commFile);
+        fclose(commFile);
+    }
+    free(path);
+    if (line) {
+        int length = strlen(line);
+        if (line[length - 1] == '\n') {
+            line[length - 1] = '\0';
+        }
+    }
+    return line;
 }
 
-// 设置格外栈空间
-void initStack() {
+char *print_sig(int sign, int code) {
+    switch (sign) {
+        case SIGFPE:
+            switch (code) {
+                case FPE_INTDIV:
+                    return "Integer divide by zero";
+                case FPE_INTOVF:
+                    return "Integer overflow";
+                case FPE_FLTDIV:
+                    return "Floating-point divide by zero";
+                case FPE_FLTOVF:
+                    return "Floating-point overflow";
+                case FPE_FLTUND:
+                    return "Floating-point underflow";
+                case FPE_FLTRES:
+                    return "Floating-point inexact result";
+                case FPE_FLTINV:
+                    return "Invalid floating-point operation";
+                case FPE_FLTSUB:
+                    return "Subscript out of range";
+                default:
+                    return "Floating-point";
+            }
+            break;
+        case SIGSEGV:
+            switch (code) {
+                case SEGV_MAPERR:
+                    return "Address not mapped to object";
+                case SEGV_ACCERR:
+                    return "Invalid permissions for mapped object";
+                default:
+                    return "Segmentation violation";
+            }
+            break;
+    }
+
+    return "None";
+}
+
+// 用于保存旧监听
+struct sigaction sa_old;
+
+/**
+ * 型号监听函数
+ *
+ * 此函数方案：
+ * 1. 新栈中
+ * 2. 新线程中
+ * 3. 新进程中（父子进程可以使用管道通信）
+ *
+ * @param code
+ * @param si
+ * @param sc
+ */
+
+static void my_handler(const int code, siginfo_t *const si, void *const sc) {
+    LOGE("sign is %d,%s", code, print_sig(si->si_signo, si->si_code));
+
+    // 设置旧的监听
+    sigaction(SIGSEGV, &sa_old, NULL);
+    // 防止循环
+    signal(code, SIG_DFL);
+    signal(SIGALRM, SIG_DFL);
+    (void) alarm(8);
+}
+
+/**
+ * 设置额外堆栈
+ */
+void init_stack() {
     stack_t stack;
-    memset(&stack, 0, sizeof(stack));
     stack.ss_size = SIGSTKSZ;
     stack.ss_sp = malloc(stack.ss_size);
     stack.ss_flags = 0;
     if (stack.ss_sp != NULL && sigaltstack(&stack, NULL) == 0) {
-
+        LOGI("%s", "stack success");
+    } else {
+        LOGE("%s", "stack failure");
     }
+}
+
+/**
+ * 信号处理监听
+ */
+void init_sigaction() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = static_cast<void (*)(int, siginfo *, void *)>(my_handler);
+    sa.sa_flags = SA_ONSTACK | SA_SIGINFO;
+    sigaction(SIGSEGV, &sa, &sa_old);// 此处保存了就监听
+}
+
+jint initCrash(JNIEnv *env, jobject jobj, jint arg) {
+    LOGI("%s", "initCrash");
+    init_stack();
+    init_sigaction();
+    return 0;
+}
+
+
+jint testCrash(JNIEnv *env, jobject jobj, jint arg) {
+    LOGI("%s", "testCrash");
+
+    return 0;
+}
+
+jint testStaticCrash(JNIEnv *jniEnv, jclass jcls, int arg) {
+    LOGI("%s", "testStaticCrash");
+    // 引起crash
+    volatile int *a = (int *) (NULL);
+    *a = 1;
+    return 0;
+}
+
+static const char *JNIUTIL_CLASS_NAME = "com/pvr/picotest/NativeInterface";
+
+static JNINativeMethod methods[] = {
+        {"testCrash",       "(I)I", (void *) testCrash},
+        {"testStaticCrash", "(I)I", (void *) testStaticCrash},
+        {"initCrash",       "(I)I", (void *) initCrash}
+};
+
+jint registerNativeMethods(JNIEnv *jniEnv, const char *class_name, JNINativeMethod *method,
+                           int num_methods) {
+    jclass targetClass = jniEnv->FindClass(class_name);
+    if (NULL != targetClass) {
+        return jniEnv->RegisterNatives(targetClass, method, num_methods);
+    }
+
+    return JNI_ERR;
+}
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    LOGI("%s", "jni init load");
+    JNIEnv *jniEnv = NULL;
+    if (JNI_OK != vm->GetEnv(reinterpret_cast<void **>(&jniEnv), JNI_VERSION_1_4)) {
+        return JNI_ERR;
+    }
+    int numbers = sizeof(methods) / sizeof(methods[0]);
+    LOGI("number=%d", numbers);
+    if (JNI_OK != registerNativeMethods(jniEnv, JNIUTIL_CLASS_NAME, methods, numbers)) {
+        LOGI("%s", "register error");
+        return JNI_ERR;
+    }
+
+    LOGI("%s", "jni init success");
+    return JNI_VERSION_1_4;
+
+}
+
+
 }
