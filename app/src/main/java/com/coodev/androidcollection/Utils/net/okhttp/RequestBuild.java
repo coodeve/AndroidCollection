7c099dbe05fd64d0dc76740633f462a102bfa9e8
patrick.ding <patrick.ding@picovr.com>
2021-02-04 11:37:42 +0800

    add multipart request

--- app/src/main/java/com/coodev/androidcollection/Utils/net/okhttp/RequestBuild.java
+++ app/src/main/java/com/coodev/androidcollection/Utils/net/okhttp/RequestBuild.java
@@ -5,6 +5,7 @@ import com.coodev.androidcollection.Utils.io.FileUtil;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.lang.invoke.MethodType;
 import java.util.Map;
 import java.util.Set;
 
@@ -27,6 +28,13 @@ public class RequestBuild {
 
     /**
      * 参数以表单形式的post请求
+     * HTTP1.1 hostname
+     * ...
+     * Content-Type: x-www-from-urlencoded
+     * ...
+     * 空行
+     * <p>
+     * key1=value1&key2=value2
      *
      * @param url
      * @param params
@@ -44,6 +52,13 @@ public class RequestBuild {
 
     /**
      * 参数以json形式发送的post请求
+     * HTTP1.1 hostname
+     * ...
+     * Content-Type: application/json
+     * ...
+     * 空行
+     * <p>
+     * json字符串
      *
      * @param url
      * @param params
@@ -56,6 +71,17 @@ public class RequestBuild {
 
     /**
      * 直接以字符串形式发送的post请求
+     * HTTP1.1 hostname
+     * ...
+     * Content-Type: application/json
+     * 选择text，则请求头是： text/plain
+     * 选择javascript，则请求头是： application/javascript
+     * 选择html，则请求头是： text/html
+     * 选择application/xml，则请求头是： application/xml
+     * ...
+     * 空行
+     * <p>
+     * 字符串
      *
      * @param url
      * @param params
@@ -68,7 +94,28 @@ public class RequestBuild {
     }
 
     /**
-     * 上传一个文件
+     * 上传一个文件，没有其他参数
+     * <p>
+     * HTTP1.1 hostname
+     * ...
+     * Content-Type: multipart/form-data;boundary=xxxooo
+     * ...
+     * 空行
+     * xxxooo
+     * Content-Description: form-data;name="test"
+     * <p>
+     * this is text data
+     * xxxooo
+     * Content-Description: form-data;name="name"
+     * <p>
+     * this is first line
+     * this is second line
+     * xxxooo
+     * Content-Description: form-data;name="file";filename="file.png"
+     * Content-Type: image/png
+     * <p>
+     * 二进制数据0100001010101001001010
+     * xxxooo--
      *
      * @param url
      * @param filePath
@@ -84,7 +131,37 @@ public class RequestBuild {
             mimeType = "application/octet-stream";
         }
         MultipartBody.Builder builder = new MultipartBody.Builder();
-        builder.addFormDataPart(fileName, fileName, RequestBody.create(MediaType.parse(mimeType), new File(filePath)));
+        builder.addFormDataPart(fileName, fileName, MultipartBody.create(new File(filePath), MediaType.parse(mimeType)));
+        MultipartBody multipartBody = builder.build();
+        return new Request.Builder().url(url).post(multipartBody).build();
+    }
+
+    /**
+     * 上传文件，携带表单参数
+     *
+     * @param url
+     * @param filePath
+     * @param mimeType
+     * @param requestParams
+     * @return
+     */
+    public Request createUploadRequest(String url, String filePath, String mimeType, Map<String, String> requestParams) {
+        String fileName = FileUtil.getFileName(filePath);
+        if (fileName == null) {
+            return null;
+        }
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        builder.setType(MultipartBody.FORM);
+        // 参数
+        for (Map.Entry<String, String> entrySet : requestParams.entrySet()) {
+            builder.addFormDataPart(entrySet.getKey(), entrySet.getValue());
+        }
+        // 文件
+        builder.addFormDataPart(fileName, fileName, MultipartBody.create(new File(filePath), MediaType.parse(mimeType)));
         MultipartBody multipartBody = builder.build();
         return new Request.Builder().url(url).post(multipartBody).build();
     }
